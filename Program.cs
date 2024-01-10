4987220e1dfa80aed34598942d4907ebf716bce3
unknown <natanoliveirasantosmed@gmail.com>
2024-01-09 22:49:56 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um objeto Nokia
+        Nokia nokia = new Nokia("123456", "Nokia 3310", "123456789012345", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Criando um objeto Iphone
+        Iphone iphone = new Iphone("654321", "Iphone 13", "543210987654321", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
